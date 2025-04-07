5d37584d536aec404879a3625cbb4d287595fc31
Durable Programming Team <djberube@durableprogramming.com>
2025-04-07 17:40:19 -0400

    rewrote named/css.rb to be sans metaprogramming to improve autocompletion/rdoc-friendliness. (thanks to @havenwood for the suggestion

--- lib/abachrome/named/css.rb
+++ lib/abachrome/named/css.rb
@@ -2,163 +2,157 @@
 
 # Colors courtesy of w3c:
 # https://www.w3.org/wiki/CSS/Properties/color/keywords
-#
+
 module Abachrome
   module Named
     module CSS
-      ColorNames = {
-        "aliceblue" => [240, 248, 255],
-        "antiquewhite" => [250, 235, 215],
-        "aqua" => [0, 255, 255],
-        "aquamarine" => [127, 255, 212],
-        "azure" => [240, 255, 255],
-        "beige" => [245, 245, 220],
-        "bisque" => [255, 228, 196],
-        "black" => [0, 0, 0],
-        "blanchedalmond" => [255, 235, 205],
-        "blue" => [0, 0, 255],
-        "blueviolet" => [138, 43, 226],
-        "brown" => [165, 42, 42],
-        "burlywood" => [222, 184, 135],
-        "cadetblue" => [95, 158, 160],
-        "chartreuse" => [127, 255, 0],
-        "chocolate" => [210, 105, 30],
-        "coral" => [255, 127, 80],
-        "cornflowerblue" => [100, 149, 237],
-        "cornsilk" => [255, 248, 220],
-        "crimson" => [220, 20, 60],
-        "cyan" => [0, 255, 255],
-        "darkblue" => [0, 0, 139],
-        "darkcyan" => [0, 139, 139],
-        "darkgoldenrod" => [184, 134, 11],
-        "darkgray" => [169, 169, 169],
-        "darkgreen" => [0, 100, 0],
-        "darkgrey" => [169, 169, 169],
-        "darkkhaki" => [189, 183, 107],
-        "darkmagenta" => [139, 0, 139],
-        "darkolivegreen" => [85, 107, 47],
-        "darkorange" => [255, 140, 0],
-        "darkorchid" => [153, 50, 204],
-        "darkred" => [139, 0, 0],
-        "darksalmon" => [233, 150, 122],
-        "darkseagreen" => [143, 188, 143],
-        "darkslateblue" => [72, 61, 139],
-        "darkslategray" => [47, 79, 79],
-        "darkslategrey" => [47, 79, 79],
-        "darkturquoise" => [0, 206, 209],
-        "darkviolet" => [148, 0, 211],
-        "deeppink" => [255, 20, 147],
-        "deepskyblue" => [0, 191, 255],
-        "dimgray" => [105, 105, 105],
-        "dimgrey" => [105, 105, 105],
-        "dodgerblue" => [30, 144, 255],
-        "firebrick" => [178, 34, 34],
-        "floralwhite" => [255, 250, 240],
-        "forestgreen" => [34, 139, 34],
-        "fuchsia" => [255, 0, 255],
-        "gainsboro" => [220, 220, 220],
-        "ghostwhite" => [248, 248, 255],
-        "gold" => [255, 215, 0],
-        "goldenrod" => [218, 165, 32],
-        "gray" => [128, 128, 128],
-        "green" => [0, 128, 0],
-        "greenyellow" => [173, 255, 47],
-        "grey" => [128, 128, 128],
-        "honeydew" => [240, 255, 240],
-        "hotpink" => [255, 105, 180],
-        "indianred" => [205, 92, 92],
-        "indigo" => [75, 0, 130],
-        "ivory" => [255, 255, 240],
-        "khaki" => [240, 230, 140],
-        "lavender" => [230, 230, 250],
-        "lavenderblush" => [255, 240, 245],
-        "lawngreen" => [124, 252, 0],
-        "lemonchiffon" => [255, 250, 205],
-        "lightblue" => [173, 216, 230],
-        "lightcoral" => [240, 128, 128],
-        "lightcyan" => [224, 255, 255],
-        "lightgoldenrodyellow" => [250, 250, 210],
-        "lightgray" => [211, 211, 211],
-        "lightgreen" => [144, 238, 144],
-        "lightgrey" => [211, 211, 211],
-        "lightpink" => [255, 182, 193],
-        "lightsalmon" => [255, 160, 122],
-        "lightseagreen" => [32, 178, 170],
-        "lightskyblue" => [135, 206, 250],
-        "lightslategray" => [119, 136, 153],
-        "lightslategrey" => [119, 136, 153],
-        "lightsteelblue" => [176, 196, 222],
-        "lightyellow" => [255, 255, 224],
-        "lime" => [0, 255, 0],
-        "limegreen" => [50, 205, 50],
-        "linen" => [250, 240, 230],
-        "magenta" => [255, 0, 255],
-        "maroon" => [128, 0, 0],
-        "mediumaquamarine" => [102, 205, 170],
-        "mediumblue" => [0, 0, 205],
-        "mediumorchid" => [186, 85, 211],
-        "mediumpurple" => [147, 112, 219],
-        "mediumseagreen" => [60, 179, 113],
-        "mediumslateblue" => [123, 104, 238],
-        "mediumspringgreen" => [0, 250, 154],
-        "mediumturquoise" => [72, 209, 204],
-        "mediumvioletred" => [199, 21, 133],
-        "midnightblue" => [25, 25, 112],
-        "mintcream" => [245, 255, 250],
-        "mistyrose" => [255, 228, 225],
-        "moccasin" => [255, 228, 181],
-        "navajowhite" => [255, 222, 173],
-        "navy" => [0, 0, 128],
-        "oldlace" => [253, 245, 230],
-        "olive" => [128, 128, 0],
-        "olivedrab" => [107, 142, 35],
-        "orange" => [255, 165, 0],
-        "orangered" => [255, 69, 0],
-        "orchid" => [218, 112, 214],
-        "palegoldenrod" => [238, 232, 170],
-        "palegreen" => [152, 251, 152],
-        "paleturquoise" => [175, 238, 238],
-        "palevioletred" => [219, 112, 147],
-        "papayawhip" => [255, 239, 213],
-        "peachpuff" => [255, 218, 185],
-        "peru" => [205, 133, 63],
-        "pink" => [255, 192, 203],
-        "plum" => [221, 160, 221],
-        "powderblue" => [176, 224, 230],
-        "purple" => [128, 0, 128],
-        "red" => [255, 0, 0],
-        "rosybrown" => [188, 143, 143],
-        "royalblue" => [65, 105, 225],
-        "saddlebrown" => [139, 69, 19],
-        "salmon" => [250, 128, 114],
-        "sandybrown" => [244, 164, 96],
-        "seagreen" => [46, 139, 87],
-        "seashell" => [255, 245, 238],
-        "sienna" => [160, 82, 45],
-        "silver" => [192, 192, 192],
-        "skyblue" => [135, 206, 235],
-        "slateblue" => [106, 90, 205],
-        "slategray" => [112, 128, 144],
-        "slategrey" => [112, 128, 144],
-        "snow" => [255, 250, 250],
-        "springgreen" => [0, 255, 127],
-        "steelblue" => [70, 130, 180],
-        "tan" => [210, 180, 140],
-        "teal" => [0, 128, 128],
-        "thistle" => [216, 191, 216],
-        "tomato" => [255, 99, 71],
-        "turquoise" => [64, 224, 208],
-        "violet" => [238, 130, 238],
-        "wheat" => [245, 222, 179],
-        "white" => [255, 255, 255],
-        "whitesmoke" => [245, 245, 245],
-        "yellow" => [255, 255, 0],
-        "yellowgreen" => [154, 205, 50]
-      }.freeze
-
-      def method_missing(name, *args, &block)
-        ColorNames[name] || super
-      end
+      def self.aliceblue; [240, 248, 255] end
+      def self.antiquewhite; [250, 235, 215] end
+      def self.aqua; [0, 255, 255] end
+      def self.aquamarine; [127, 255, 212] end
+      def self.azure; [240, 255, 255] end
+      def self.beige; [245, 245, 220] end
+      def self.bisque; [255, 228, 196] end
+      def self.black; [0, 0, 0] end
+      def self.blanchedalmond; [255, 235, 205] end
+      def self.blue; [0, 0, 255] end
+      def self.blueviolet; [138, 43, 226] end
+      def self.brown; [165, 42, 42] end
+      def self.burlywood; [222, 184, 135] end
+      def self.cadetblue; [95, 158, 160] end
+      def self.chartreuse; [127, 255, 0] end
+      def self.chocolate; [210, 105, 30] end
+      def self.coral; [255, 127, 80] end
+      def self.cornflowerblue; [100, 149, 237] end
+      def self.cornsilk; [255, 248, 220] end
+      def self.crimson; [220, 20, 60] end
+      def self.cyan; [0, 255, 255] end
+      def self.darkblue; [0, 0, 139] end
+      def self.darkcyan; [0, 139, 139] end
+      def self.darkgoldenrod; [184, 134, 11] end
+      def self.darkgray; [169, 169, 169] end
+      def self.darkgreen; [0, 100, 0] end
+      def self.darkgrey; [169, 169, 169] end
+      def self.darkkhaki; [189, 183, 107] end
+      def self.darkmagenta; [139, 0, 139] end
+      def self.darkolivegreen; [85, 107, 47] end
+      def self.darkorange; [255, 140, 0] end
+      def self.darkorchid; [153, 50, 204] end
+      def self.darkred; [139, 0, 0] end
+      def self.darksalmon; [233, 150, 122] end
+      def self.darkseagreen; [143, 188, 143] end
+      def self.darkslateblue; [72, 61, 139] end
+      def self.darkslategray; [47, 79, 79] end
+      def self.darkslategrey; [47, 79, 79] end
+      def self.darkturquoise; [0, 206, 209] end
+      def self.darkviolet; [148, 0, 211] end
+      def self.deeppink; [255, 20, 147] end
+      def self.deepskyblue; [0, 191, 255] end
+      def self.dimgray; [105, 105, 105] end
+      def self.dimgrey; [105, 105, 105] end
+      def self.dodgerblue; [30, 144, 255] end
+      def self.firebrick; [178, 34, 34] end
+      def self.floralwhite; [255, 250, 240] end
+      def self.forestgreen; [34, 139, 34] end
+      def self.fuchsia; [255, 0, 255] end
+      def self.gainsboro; [220, 220, 220] end
+      def self.ghostwhite; [248, 248, 255] end
+      def self.gold; [255, 215, 0] end
+      def self.goldenrod; [218, 165, 32] end
+      def self.gray; [128, 128, 128] end
+      def self.green; [0, 128, 0] end
+      def self.greenyellow; [173, 255, 47] end
+      def self.grey; [128, 128, 128] end
+      def self.honeydew; [240, 255, 240] end
+      def self.hotpink; [255, 105, 180] end
+      def self.indianred; [205, 92, 92] end
+      def self.indigo; [75, 0, 130] end
+      def self.ivory; [255, 255, 240] end
+      def self.khaki; [240, 230, 140] end
+      def self.lavender; [230, 230, 250] end
+      def self.lavenderblush; [255, 240, 245] end
+      def self.lawngreen; [124, 252, 0] end
+      def self.lemonchiffon; [255, 250, 205] end
+      def self.lightblue; [173, 216, 230] end
+      def self.lightcoral; [240, 128, 128] end
+      def self.lightcyan; [224, 255, 255] end
+      def self.lightgoldenrodyellow; [250, 250, 210] end
+      def self.lightgray; [211, 211, 211] end
+      def self.lightgreen; [144, 238, 144] end
+      def self.lightgrey; [211, 211, 211] end
+      def self.lightpink; [255, 182, 193] end
+      def self.lightsalmon; [255, 160, 122] end
+      def self.lightseagreen; [32, 178, 170] end
+      def self.lightskyblue; [135, 206, 250] end
+      def self.lightslategray; [119, 136, 153] end
+      def self.lightslategrey; [119, 136, 153] end
+      def self.lightsteelblue; [176, 196, 222] end
+      def self.lightyellow; [255, 255, 224] end
+      def self.lime; [0, 255, 0] end
+      def self.limegreen; [50, 205, 50] end
+      def self.linen; [250, 240, 230] end
+      def self.magenta; [255, 0, 255] end
+      def self.maroon; [128, 0, 0] end
+      def self.mediumaquamarine; [102, 205, 170] end
+      def self.mediumblue; [0, 0, 205] end
+      def self.mediumorchid; [186, 85, 211] end
+      def self.mediumpurple; [147, 112, 219] end
+      def self.mediumseagreen; [60, 179, 113] end
+      def self.mediumslateblue; [123, 104, 238] end
+      def self.mediumspringgreen; [0, 250, 154] end
+      def self.mediumturquoise; [72, 209, 204] end
+      def self.mediumvioletred; [199, 21, 133] end
+      def self.midnightblue; [25, 25, 112] end
+      def self.mintcream; [245, 255, 250] end
+      def self.mistyrose; [255, 228, 225] end
+      def self.moccasin; [255, 228, 181] end
+      def self.navajowhite; [255, 222, 173] end
+      def self.navy; [0, 0, 128] end
+      def self.oldlace; [253, 245, 230] end
+      def self.olive; [128, 128, 0] end
+      def self.olivedrab; [107, 142, 35] end
+      def self.orange; [255, 165, 0] end
+      def self.orangered; [255, 69, 0] end
+      def self.orchid; [218, 112, 214] end
+      def self.palegoldenrod; [238, 232, 170] end
+      def self.palegreen; [152, 251, 152] end
+      def self.paleturquoise; [175, 238, 238] end
+      def self.palevioletred; [219, 112, 147] end
+      def self.papayawhip; [255, 239, 213] end
+      def self.peachpuff; [255, 218, 185] end
+      def self.peru; [205, 133, 63] end
+      def self.pink; [255, 192, 203] end
+      def self.plum; [221, 160, 221] end
+      def self.powderblue; [176, 224, 230] end
+      def self.purple; [128, 0, 128] end
+      def self.red; [255, 0, 0] end
+      def self.rosybrown; [188, 143, 143] end
+      def self.royalblue; [65, 105, 225] end
+      def self.saddlebrown; [139, 69, 19] end
+      def self.salmon; [250, 128, 114] end
+      def self.sandybrown; [244, 164, 96] end
+      def self.seagreen; [46, 139, 87] end
+      def self.seashell; [255, 245, 238] end
+      def self.sienna; [160, 82, 45] end
+      def self.silver; [192, 192, 192] end
+      def self.skyblue; [135, 206, 235] end
+      def self.slateblue; [106, 90, 205] end
+      def self.slategray; [112, 128, 144] end
+      def self.slategrey; [112, 128, 144] end
+      def self.snow; [255, 250, 250] end
+      def self.springgreen; [0, 255, 127] end
+      def self.steelblue; [70, 130, 180] end
+      def self.tan; [210, 180, 140] end
+      def self.teal; [0, 128, 128] end
+      def self.thistle; [216, 191, 216] end
+      def self.tomato; [255, 99, 71] end
+      def self.turquoise; [64, 224, 208] end
+      def self.violet; [238, 130, 238] end
+      def self.wheat; [245, 222, 179] end
+      def self.white; [255, 255, 255] end
+      def self.whitesmoke; [245, 245, 245] end
+      def self.yellow; [255, 255, 0] end
+      def self.yellowgreen; [154, 205, 50] end
     end
   end
 end
